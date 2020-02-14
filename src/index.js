dedcbc6d036c75d5b42f02c5ce77510404ca966a
FyuroTEX <yurilev4@gmail.com>
2020-02-14 03:47:35 +0200

    fetch

--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function App(){
+function App() {
+    const getResource = async (url) => {
+        const res = await fetch(url);
+        if (!res.ok) {
+            throw new Error(`could not fetch ${url}, received ${res.status}`)
+        }
+        const body = await res.json();
+        return body;
+    };
+    getResource('https://swapi.co/api/people/1/')
+        .then((body) => {
+            console.log(body);
+        })
+        .catch((err) => {
+            console.error('could not fetch', err);
+        });
+
+    // fetch('https://swapi.co/api/people/1/')
+    //     .then((res) => {
+    //         return res.json();
+    //     })
+    //     .then((body) => {
+    //         console.log(body);
+    //     });
     console.log('Hello World');
     return <h1>Hello World!</h1>
 }
