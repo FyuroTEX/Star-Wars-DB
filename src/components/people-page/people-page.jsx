a467fd8fab763b539384c2fa55a2d7297e17b8c0
FyuroTEX <yurilev4@gmail.com>
2020-02-20 00:50:29 +0200

    props.children

--- src/components/people-page/people-page.jsx
+++ src/components/people-page/people-page.jsx
@@ -3,8 +3,9 @@ import SwapiService from '../../services/swapi-service';
 import ErrorIndicator from '../error-indicator/error-indicator';
 import ItemList from '../item-list/item-list';
 import PersonDetails from '../person-details/person-details';
+import Row from '../row';
+import ErrorBoundry from '../error-boundry';
 import './people-page.css';
-import Row from '../Row';
 
 
 
@@ -14,16 +15,10 @@ import Row from '../Row';
 export default class PeoplePage extends Component {
   swapiService = new SwapiService();
   state = {
-    selectedPerson: null,
-    hasError: false
+    selectedPerson: 2,
   };
 
-  componentDidCatch(error, info) {
 
-    this.setState({
-      hasError: true
-    });
-  };
 
   onPersonSelected = (selectedPerson) => {
     this.setState({ selectedPerson });
@@ -38,16 +33,18 @@ export default class PeoplePage extends Component {
     const itemList = (
       <ItemList
         onItemSelected={this.onPersonSelected}
-        getData={this.swapiService.getAllPeople}> 
+        getData={this.swapiService.getAllPeople}>
         {(i) => `${i.name} ( ${i.birthYear} )`}
-        </ItemList>
+      </ItemList>
     );
     const personDetails = (
       <PersonDetails personId={this.state.selectedPerson} />
     );
 
     return (
-      <Row left={itemList} right={personDetails} />
+      <ErrorBoundry>
+        <Row left={itemList} right={personDetails} />
+      </ErrorBoundry>
     );
   };
 };
