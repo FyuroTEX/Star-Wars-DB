db6521d0a70fb13b5abbd099f10756370246cc12
FyuroTEX <yurilev4@gmail.com>
2020-02-27 00:44:39 +0200

    change PeoplePage

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -37,7 +37,7 @@ export default class App extends Component {
               <Header onServiceChange={this.onServiceChange} />
               <RandomPlanet />
               <Route path='/' exact render={() => <h2>Welcome to StarDB</h2>} />
-              <Route path='/people' component={PeoplePage} />
+              <Route path='/people/:id?' component={PeoplePage} />
               <Route path='/planets' component={PlanetPage} />
               <Route path='/starships' exact component={StarshipPage} />
               <Route path='/starships/:id'
