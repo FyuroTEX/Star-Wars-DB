adb41b44f743ce3d0908b6e7ca589ef6ac684648
FyuroTEX <yurilev4@gmail.com>
2020-02-27 16:38:06 +0200

    pre Redux

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import DummySwapiService from '../../services/dummy-swapi-service';
 import SwapiService from '../../services/swapi-service';
 import ErrorBoundry from '../error-boundry';
@@ -62,7 +62,7 @@ export default class App extends Component {
               <RandomPlanet />
               <Switch>
 
-                <Route path='/' exact render={() => <h2>Welcome to StarDB</h2>} />
+                <Route path='/' exact render={() => <h2 className='text-center'>Welcome to StarDB</h2>} />
                 <Route path='/people/:id?' component={PeoplePage} />
                 <Route path='/planets' component={PlanetPage} />
                 <Route path='/starships' exact component={StarshipPage} />
@@ -82,9 +82,8 @@ export default class App extends Component {
                 <Route path="/secret"
                   render={() => <SecretPage isLoggedIn={isLoggedIn} />} />
 
-                <Route render={() => <h2>Page not found</h2>} /> 
-                {/* or use  */}
-                <Redirect to='/' />
+                <Route render={() => <h2 className='text-center'>Page not found</h2>} />
+
 
               </Switch>
             </div>
