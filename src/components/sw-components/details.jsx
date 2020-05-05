99e8005bf1ba67a8c569aa6eb7ccc6f93dc66298
FyuroTEX <yurilev4@gmail.com>
2020-05-05 22:45:12 +0300

    change API to swapi.dev

--- src/components/sw-components/details.jsx
+++ src/components/sw-components/details.jsx
@@ -3,77 +3,58 @@ import ItemDetails, { Record } from '../item-details/item-details';
 // import SwapiService from '../../services/swapi-service';
 import { SwapiServiceConsumer } from '../swapi-service-context';
 
-
-
 const PersonDetails = ({ itemId }) => {
     return (
         <SwapiServiceConsumer>
-            {
-                ({ getPerson, getPersonImage }) => {
-                    return (
-                        <ItemDetails
-                            itemId={itemId}
-                            getData={getPerson}
-                            getImageUrl={getPersonImage} >
-
-                            <Record field="gender" label="Gender" />
-                            <Record field="eyeColor" label="Eye Color" />
-
-                        </ItemDetails>
-                    );
-                }
-            }
+            {({ getPerson, getPersonImage }) => {
+                return (
+                    <ItemDetails
+                        itemId={itemId}
+                        getData={getPerson}
+                        getImageUrl={getPersonImage}>
+                        <Record field="gender" label="Gender" />
+                        <Record field="eyeColor" label="Eye Color" />
+                    </ItemDetails>
+                );
+            }}
         </SwapiServiceConsumer>
     );
 };
 const PlanetDetails = ({ itemId }) => {
     return (
         <SwapiServiceConsumer>
-            {
-                ({ getPlanet, getPlanetImage }) => {
-                    return (
-                        <ItemDetails
-                            itemId={itemId}
-                            getData={getPlanet}
-                            getImageUrl={getPlanetImage} >
-
-                            <Record field="population" label="Population" />
-                            <Record field="rotationPeriod" label="Rotation period" />
-                            <Record field="diameter" label="Diameter" />
-                        </ItemDetails>
-                    );
-                }
-            }
+            {({ getPlanet, getPlanetImage }) => {
+                return (
+                    <ItemDetails
+                        itemId={itemId}
+                        getData={getPlanet}
+                        getImageUrl={getPlanetImage}>
+                        <Record field="population" label="Population" />
+                        <Record field="rotationPeriod" label="Rotation period" />
+                        <Record field="diameter" label="Diameter" />
+                    </ItemDetails>
+                );
+            }}
         </SwapiServiceConsumer>
-
     );
 };
 const StarshipDetails = ({ itemId }) => {
     return (
-
         <SwapiServiceConsumer>
-            {
-                (swapiService) => {
-                    return (
-                        <ItemDetails
-                            itemId={itemId}
-                            getData={swapiService.getStarship}
-                            getImageUrl={swapiService.getStarshipImage} >
-
-                            <Record field="model" label="Model" />
-                            <Record field="length" label="Length" />
-                            <Record field="costInCredits" label="Cost" />
-                        </ItemDetails>
-                    );
-                }
-            }
+            {(swapiService) => {
+                return (
+                    <ItemDetails
+                        itemId={itemId}
+                        getData={swapiService.getStarship}
+                        getImageUrl={swapiService.getStarshipImage}>
+                        <Record field="model" label="Model" />
+                        <Record field="length" label="Length" />
+                        <Record field="costInCredits" label="Cost" />
+                    </ItemDetails>
+                );
+            }}
         </SwapiServiceConsumer>
-
     );
 };
 
-export {
-    PersonDetails,
-    PlanetDetails,
-    StarshipDetails
-};
+export { PersonDetails, PlanetDetails, StarshipDetails };
