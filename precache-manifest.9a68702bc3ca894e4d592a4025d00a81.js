af1ceb426b64cb74549b033b3da32173fa085676
FyuroTEX <yurilev4@gmail.com>
2020-06-30 00:01:58 +0300

    Updates

---
+++ precache-manifest.9a68702bc3ca894e4d592a4025d00a81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2d24028116bc318160e9f1c9a0a4a85",
+    "url": "/star-wars-db/index.html"
+  },
+  {
+    "revision": "4a653c4cc6d21f1c738a",
+    "url": "/star-wars-db/static/css/main.e68aa6de.chunk.css"
+  },
+  {
+    "revision": "d9d38c094df4dcd658d0",
+    "url": "/star-wars-db/static/js/2.8e1b5198.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/star-wars-db/static/js/2.8e1b5198.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a653c4cc6d21f1c738a",
+    "url": "/star-wars-db/static/js/main.87b5f431.chunk.js"
+  },
+  {
+    "revision": "ff2aa5379c899b63759e",
+    "url": "/star-wars-db/static/js/runtime-main.d34032ff.js"
+  }
+]);
